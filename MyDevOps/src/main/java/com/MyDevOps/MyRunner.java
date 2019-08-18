11a787cd0e0a7320a7d88b2f5a2591f7556a5428
sakthivel74 <sakthivel_p@hotmail.com>
2019-08-18 15:07:35 +0800

    First commit from Eclipse

---
+++ MyDevOps/src/main/java/com/MyDevOps/MyRunner.java
@@ -0,0 +1,22 @@
+package com.MyDevOps;
+
+public class MyRunner {
+
+	
+	public int sum(int x, int y) {
+		return x+y;
+	}
+	
+	
+	public String sayHello() {
+		return "Hello!!!";
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	
+	
+}
